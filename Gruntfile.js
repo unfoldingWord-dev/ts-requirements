9e8c903329a20142294ba7d24be26e7e57c51d69
Joel Lonbeck <joel@neutrinographics.com>
2015-06-17 14:59:01 -0700

    added build system to quickly concat all features into a portable file

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: '\n\n'
+      },
+      dist: {
+        src: ['features/*.feature'],
+        dest: 'build/requirements.txt'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat']);
+
+};
